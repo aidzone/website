be38f6a2301b390ddcbdd0353f593703ed807ff7
Barkin Simsek <barkin.simsek@hotmail.com>
2020-05-12 02:56:58 -0400

    added the main map

---
+++ app/Views/pages/home.php
@@ -0,0 +1,30 @@
+<div id="map" class="map"></div>
+<div id="popup" class="ol-popup">
+    <a href="#" id="popup-closer" class="ol-popup-closer"></a>
+    <div id="popup-content"></div>
+</div>
+<script type="text/javascript">
+    var map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([38.221, 36.914]),
+            zoom: 4
+        })
+    });
+
+    var layer = new ol.layer.Vector({
+        source: new ol.source.Vector({
+            features: [
+                new ol.Feature({
+                    geometry: new ol.geom.Point(ol.proj.fromLonLat([32.221, 38.914]))
+                })
+            ]
+        })
+    });
+    map.addLayer(layer);
+</script>
---
+++ app/Views/templates/header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+    <title>AidZone - <?= esc($title); ?></title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.3.1/css/ol.css" type="text/css">
+    <style>
+        html, body {
+            height: 100%;
+            padding: 0;
+            margin: 0;
+        }
+        
+        .map {
+            height: 100%;
+            width: 100vw;
+        }
+    </style>
+    <script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.3.1/build/ol.js"></script>
+</head>
+<body>
